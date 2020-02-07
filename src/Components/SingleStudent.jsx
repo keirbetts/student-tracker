f1290fbd3fc1289035e3f145cbbb80f99967b3ca
Hy-M <humayraa.m@outlook.com>
2020-02-07 11:07:15 +0000

    adding block repeats functionality

--- src/Components/SingleStudent.jsx
+++ src/Components/SingleStudent.jsx
@@ -22,14 +22,64 @@ class SingleStudent extends Component {
       <div>
         <h3>{name}</h3>
         <p>StartingCohort: {startingCohort}</p>
-        {blockHistory.map(block => {
-          return (
-            <div key={block._id}>
-              <p>Block: {block.name}</p>
-              <p>BlockNumber: {block.number}</p>
-            </div>
-          );
-        })}
+        {blockHistory.map((block, index) => {
+          let count = 0;
+
+          if (blockHistory[index + 1]) {
+            if (block.name === blockHistory[index + 1].name) {
+              count++;
+              if (index !== blockHistory.length - 1) {
+                return (
+                  <div key={index}>
+                    <p>Block: {block.name}</p>
+                    <p>BlockNumber: {block.number}</p>
+                     <p>Block repeats: {count}</p>
+                  </div>
+                ); 
+              } else {
+                return (
+                  <div key={block._id}>
+                    <p>Block: {block.name}</p>
+                    <p>BlockNumber: {block.number}</p>
+                  </div>
+                );
+              }
+            } else {
+              if (index !== blockHistory.length - 1) {
+                return (
+                  <div key={index}>
+                    <p>Block: {block.name} 	&#10003;</p>
+                    <p>BlockNumber: {block.number}</p>
+                    <p>Block repeats: {count}</p>
+                  </div>
+                ); 
+              } else {
+                return (
+                  <div key={block._id}>
+                    <p>Block: {block.name}</p>
+                    <p>BlockNumber: {block.number}</p>
+                    <p>Block repeats: {count}</p>
+                  </div>
+                );
+              }
+          }
+          
+          // if (index !== blockHistory.length - 1) {
+          // return (
+          //   <div key={index}>
+          //     <p>Block: {block.name} 	&#10003;</p>
+          //     <p>BlockNumber: {block.number}</p>
+          //   </div>
+          // );
+          // } else {
+          //   return (
+          //     <div key={block._id}>
+          //       <p>Block: {block.name}</p>
+          //       <p>BlockNumber: {block.number}</p>
+          //     </div>
+          //   );
+          // }         
+        }})}
         <p>student_id: {_id}</p>
       </div>
     );
