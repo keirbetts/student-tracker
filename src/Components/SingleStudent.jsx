5d771c74d93ed5e7cb9455095fbcb3962455e69a
Hy-M <humayraa.m@outlook.com>
2020-02-07 14:03:55 +0000

    editing single student

--- src/Components/SingleStudent.jsx
+++ src/Components/SingleStudent.jsx
@@ -17,69 +17,28 @@ class SingleStudent extends Component {
   };
 
   render() {
+    let repeatsCount = 0;
+    let timeTaken = 0; 
+    let repeatsTag = <p>Block repeats: {repeatsCount}</p>
     const { name, startingCohort, blockHistory, _id } = this.state.student;
     return (
       <div>
         <h3>{name}</h3>
         <p>StartingCohort: {startingCohort}</p>
         {blockHistory.map((block, index) => {
-          let count = 0;
-
-          if (blockHistory[index + 1]) {
-            if (block.name === blockHistory[index + 1].name) {
-              count++;
-              if (index !== blockHistory.length - 1) {
-                return (
-                  <div key={index}>
-                    <p>Block: {block.name}</p>
-                    <p>BlockNumber: {block.number}</p>
-                     <p>Block repeats: {count}</p>
-                  </div>
-                ); 
-              } else {
-                return (
-                  <div key={block._id}>
-                    <p>Block: {block.name}</p>
-                    <p>BlockNumber: {block.number}</p>
-                  </div>
-                );
+            if (blockHistory[index + 1]) {
+              if (block.name === blockHistory[index + 1].name) {
+                repeatsCount++;
               }
-            } else {
-              if (index !== blockHistory.length - 1) {
-                return (
-                  <div key={index}>
-                    <p>Block: {block.name} 	&#10003;</p>
-                    <p>BlockNumber: {block.number}</p>
-                    <p>Block repeats: {count}</p>
-                  </div>
-                ); 
-              } else {
-                return (
-                  <div key={block._id}>
-                    <p>Block: {block.name}</p>
-                    <p>BlockNumber: {block.number}</p>
-                    <p>Block repeats: {count}</p>
-                  </div>
-                );
-              }
-          }
-          
-          // if (index !== blockHistory.length - 1) {
-          // return (
-          //   <div key={index}>
-          //     <p>Block: {block.name} 	&#10003;</p>
-          //     <p>BlockNumber: {block.number}</p>
-          //   </div>
-          // );
-          // } else {
-          //   return (
-          //     <div key={block._id}>
-          //       <p>Block: {block.name}</p>
-          //       <p>BlockNumber: {block.number}</p>
-          //     </div>
-          //   );
-          // }         
-        }})}
+            }
+            return (
+              <div key={index}>
+              <p>Block: {block.name}{index !== blockHistory.length - 1 && <span>&#10003;</span>}</p> 
+                {/* <p>BlockNumber: {block.number}</p> */}
+                <p>Attempts: {repeatsCount}</p>
+              </div>
+            )
+        })}
         <p>student_id: {_id}</p>
       </div>
     );
@@ -87,3 +46,43 @@ class SingleStudent extends Component {
 }
 
 export default SingleStudent;
+
+
+// if (blockHistoryS[index + 1]) {
+  //   if (block.name === blockHistory[index + 1].name) {
+  //     count++;
+  //     if (index !== blockHistory.length - 1) {
+  //       return (
+  //         <div key={index}>
+  //           <p>Block: {block.name}</p>
+  //           <p>BlockNumber: {block.number}</p>
+  //            <p>Block repeats: {count}</p>
+  //         </div>
+  //       ); 
+  //     } else {
+  //       return (
+  //         <div key={block._id}>
+  //           <p>Block: {block.name}</p>
+  //           <p>BlockNumber: {block.number}</p>
+  //         </div>
+  //       );
+  //     }
+  //   } else {
+  //     if (index !== blockHistory.length - 1) {
+  //       return (
+  //         <div key={index}>
+  //           <p>Block: {block.name} 	&#10003;</p>
+  //           <p>BlockNumber: {block.number}</p>
+  //           <p>Block repeats: {count}</p>
+  //         </div>
+  //       ); 
+  //     } else {
+  //       return (
+  //         <div key={block._id}>
+  //           <p>Block: {block.name}</p>
+  //           <p>BlockNumber: {block.number}</p>
+  //           <p>Block repeats: {count}</p>
+  //         </div>
+  //       );
+  //     }
+  // }
